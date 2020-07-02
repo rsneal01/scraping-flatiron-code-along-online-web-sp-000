95b83520be1aaa6f10909b21f62558eb861b8063
Bobby Neal <rsneal01@gmail.com>
2020-07-02 18:22:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,6 +21,10 @@ class Scraper
     self.get_page.css(".post")
   end
   
+  def make_courses
+    self.get_courses.each do |post|
+      
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
